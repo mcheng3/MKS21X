a9620e1dd1e2b423cb090439dd845ab50482f8b7
Michael Cheng <Michael Cheng>
2016-11-30 21:25:19 -0500

    Update Book.java

--- 01Library/Book.java
+++ 01Library/Book.java
@@ -1,3 +1,33 @@
 public class Book{
-	PEIFJDKJF:LSDJFO:EIFJEO:SIFJf
+	private String author;
+	private String title;
+	private String isbn;
+
+	public Book(){
+
+	}
+	
+	public Book(String author, String title, String isbn){
+		this.author = author;
+		this.title = title;
+		thils.isbn = isbn;	
+	}
+
+	public String getAuthor(){
+		return author;
+	}
+
+	public String getTitle(){
+		return title;
+	}
+
+	public String getISBN(){
+		return isbn;
+	}
+
+	public String toString(){
+		return title + ", " + author + ", " + isbn;
+	}
+
+
 }
