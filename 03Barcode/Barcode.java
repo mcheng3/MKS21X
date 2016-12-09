f565f1a07c490a2d566549b4c9fd3511e9d65b5f
mcheng3 <michael.cheng65@gmail.com>
2016-12-09 03:41:58 -0500

    Edit Barcode.java

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -6,6 +6,15 @@ public class Barcode{
 	_zip = zip;
 	checkSum();
     }
+
+    public String toString(){
+	String code = "|";
+	for(int i = 0; i< _zip.length(); i++){
+	    switch(_zip.charAt(i) - '0'){
+	    case 1: code += ":::||";
+      	}
+	return code + ;
+    }
     
     public Barcode clone(){
 	return this;
@@ -14,15 +23,24 @@ public class Barcode{
     private int checkSum(){
 	int sum = 0;
 	for(int i = 0; i< _zip.length() - 1; i++){
-	    sum += Integer.parseInt(_zip.substring(i, i+1));
+	    sum += _zip.charAt(i) - '0';
       	}
 	System.out.println(sum);
 	return sum % 10;
     }
     
+    /*public static String toCode(String zip){
+	for(int i = 0; i< _zip.length(); i++){
+	   
+      	}
+	return "";
+	}*/
+	
     public static void main(String[] args){
-	Barcode a = new Barcode("01005");
+	Barcode a = new Barcode("01305");
     }
+
+   
 }
 
 
