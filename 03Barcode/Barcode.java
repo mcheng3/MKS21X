3e26322ebf48f2f3ac46a89dbdb1faef30f5310c
mcheng3 <michael.cheng65@gmail.com>
2016-12-12 10:37:27 -0500

    Update Barcode.java

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -3,30 +3,44 @@ public class Barcode{
     private int _checkDigit;
 
     public Barcode(String zip){
-	_zip = zip;
-	checkSum();
+		_zip = zip;
+		checkSum();
     }
 
-    public String toString(){
-	String code = "|";
-	for(int i = 0; i< _zip.length(); i++){
-	    switch(_zip.charAt(i) - '0'){
-	    case 1: code += ":::||";
-      	}
-	return code + ;
-    }
-    
+    public String toCode(){
+		String code = "|";
+		for(int i = 0; i< _zip.length(); i++){
+	    	switch(_zip.charAt(i) - '0'){
+	    		case 1: code += ":::||";
+				case 2: code += "::|:|";
+				case 3: code += "::||:";
+				case 4: code += ":|::|";
+				case 5: code += ":|:|:";
+				case 6: code += ":||::";
+				case 7: code += "|:::|";
+				case 8: code += "|::|:";
+				case 9: code += "|:|::";
+				case 0: code += "||:::";
+		    }
+  	    }
+		return code + checkSum() + "|";
+ 	}
+	
+	public String toString(){
+		return _zip + " " + toCode();
+	}
+	
     public Barcode clone(){
-	return this;
+		return this;
     }
     
     private int checkSum(){
-	int sum = 0;
-	for(int i = 0; i< _zip.length() - 1; i++){
-	    sum += _zip.charAt(i) - '0';
-      	}
-	System.out.println(sum);
-	return sum % 10;
+		int sum = 0;
+		for(int i = 0; i< _zip.length() - 1; i++){
+		    sum += _zip.charAt(i) - '0';
+    	  	}
+		System.out.println(sum);
+		return sum % 10;
     }
     
     /*public static String toCode(String zip){
@@ -37,7 +51,7 @@ public class Barcode{
 	}*/
 	
     public static void main(String[] args){
-	Barcode a = new Barcode("01305");
+		Barcode a = new Barcode("01305");
     }
 
    
