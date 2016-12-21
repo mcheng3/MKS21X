8a37320a9e3a4917fa9b2ce16bdc3b5be6df5783
mcheng3 <michael.cheng65@gmail.com>
2016-12-21 10:59:08 -0500

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -1,1 +1,19 @@
-
+public class Temperature{
+    public static double CToF(double t){
+        int f = 0;
+        f = 1.8 * t + 32;
+        return f;
+    }
+    
+    public static double FToC(double t){
+        int c = 0;
+        c = (t - 32) / 1.8;
+        return c;
+    }
+    
+    public static void main(String[] args){
+        System.out.println(Temperature.CToF(100.0));
+        System.out.println(Temperature.FToC(32.0));
+}
+    
+      
