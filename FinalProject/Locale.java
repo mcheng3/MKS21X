c8e5d8e773671c154588e096609366d4980ee633
Michael Cheng <Michael Cheng>
2017-01-09 21:30:02 -0500

    Add JSON parsing to Weather.java

--- FinalProject/Locale.java
+++ FinalProject/Locale.java
@@ -2,21 +2,31 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 import java.text.SimpleDateFormat;
+import org.json.*;
+
 public class Locale{
-	private String weatherJSON;
+	private JSONObject weatherJSON;
+	private Calendar cal;
+	private String state;
+	private String city;
 
 	public Locale(int year, int month, int day, String state, String city){
-		int gddSum = 0;
-		city = city.replaceAll(" ", "_");
+		this.city = city.replaceAll(" ", "_");
+		this.state = state;
 		month = month - 1;
-		Calendar cal = new GregorianCalendar(year, month, day);
+		cal = new GregorianCalendar(year, month, day);
+		weatherJSON = new JSONObject(getData());
+
+	}
+
+	public String getData(){
 		Date startDate = cal.getTime();
 		cal.add(Calendar.DAY_OF_MONTH, 7);
 		Date endDate = cal.getTime();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("MMdd");
 		String start = dateFormat.format(startDate);
 		String end = dateFormat.format(endDate);
-		String  content = "";
+		String content = "";
 		try{
 			URL apiURL = new URL("http://api.wunderground.com/api/0eda91f94dd93a2c/planner_"+start+end+"/q/"+state+"/"+city+".json");
 			URLConnection apiURLConnection = apiURL.openConnection();
@@ -31,13 +41,12 @@ public class Locale{
 		catch(Exception e){
 			e.printStackTrace();
 		}
-		weatherJSON = content;
-
+		return content;
 	}
 	//http://api.wunderground.com/api/0eda91f94dd93a2c/planner_MMDDMMDD/q/CA/San_Francisco.json
-	public static int daysForGDD(){
-
-		return 0; 
+	public String daysForGDD(int requiredGDD){
+		int gddSum = 0;
+		return weatherJSON.getJSONObject("trip").getJSONObject("temp_high").getJSONObject("avg").getString("F"); 
 	}
 
 	public static int daysForPreciptation(){
@@ -46,6 +55,7 @@ public class Locale{
 
 	public static void main(String[] args){
 		Locale nyc = new Locale(2016, 5, 9, "NY", "New York");
+		System.out.println(nyc.daysForGDD(2000));
 	}
 }
 
